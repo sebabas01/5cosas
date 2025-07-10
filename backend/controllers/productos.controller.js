490fc2cd515edfddeda019a1210f57e5f02e0052
Sebastian Badillo Romo <utm21040128@utma.edu.mx>
2025-07-10 16:55:48 -0600

    trabajo del 10 de julio

--- backend/controllers/productos.controller.js
+++ backend/controllers/productos.controller.js
@@ -1,2 +1,2 @@
-import { modeloPalabras } from "../models/palabras.model.js";
+import { modeloPalabras } from "../models/palabras.model.js"; // importa "modelopalabras" desde el archivo palabras.model.js que se encuentra en models
 
--- backend/models/productos.model.js
+++ backend/models/productos.model.js
@@ -1,9 +1,31 @@
-import {Schema, model} from "mongoose"
-import { type } from "os"
+import mongoose from "mongoose" // importa la libreria mongoose que permite interactuar con la base de datos
+import { Schema, model } from "mongoose"// extrae los nodelos "schema" y "model" de mongoose para crear y definir modelos de datos
 
-const EsquemaPalabras = new Schema({
-    palabras:String
-
+const EsquemaProductos = new Schema({ //define un nuevo esquema para los productos
+    nombre: {
+        type: String, // el campo "nombre" debe ser con letras
+        required: true //valida que tenga contenido y no se quede vacío
+    },
+    precio: {
+        type: Number,//el campo "precio" debe ser un número
+        required: true//valida que tenga contenido y no se quede vacío
+    }
 })
 
-export const modeloPalabras = new model("Palabras", EsquemaPalabras)
+const modeloProductos = model("Productos", EsquemaProductos)// crea un modelo "productos" basado en el esquema anterior
+const productos = [//crea un arreglo de objetos cada uno con nombre y precio 
+    { nombre: "Creatina Monohidratada", precio: 450 },
+    { nombre: "Proteína Whey 2kg", precio: 950 },
+    { nombre: "Multivitamínico Hombre", precio: 299 },
+    { nombre: "BCAA 2:1:1", precio: 380 },
+    { nombre: "Óxido Nítrico Pre-entreno", precio: 520 }
+]
+modeloProductos.insertMany(productos)//inserta todos los productos en la base de datos utilizando insertmany
+    .then(() => {
+        console.log(" Productos insertados correctamente")//si se einsertaron de manera correcta se mostrara el mensaje "Productos insertados correctamente"
+        mongoose.connection.close() // cierra la conexión con la base de datos
+    })
+    .catch(err => {
+        console.error("Error al insertar productos:", err)// si la inserción no se completa se mostará el mensaje "Error al insertar productos:"
+        mongoose.connection.close()//cierra la base de datos
+    })
